b16dbcc2757333c72962ef54373d9cff8a86ed6d
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-09-17 13:26:54 -0300

    Adicionado analytics vercel

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import "@fontsource/exo-2/800.css"; // Specify weight
 import "@fontsource/exo-2/800-italic.css"; // Specify weight and style
 import "@fontsource/pt-sans"; // Defaults to weight 400
 import Home from './components/Home.vue'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 </script>
 
 <template>
